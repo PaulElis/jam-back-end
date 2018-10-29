d2ab63075b4ae8dd277331f3cb3a51b5aad0c9c3
github username <elis.paul7@gmail.com>
2018-10-29 10:48:09 -0400

    first commit

---
+++ app/controllers/api/v1/albums_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::AlbumsController < ApplicationController
+
+  def index
+    @albums = Album.all
+    render json: @albums
+  end
+
+  def create
+    @album = Album.find_or_create_by(
+      name: params['name'],
+      image: params['image'][3]['#text'],
+      playcount: params['playcount'],
+      url: params['url'],
+      artistId: params['artist']['mbid'],
+    )
+    if @album.save
+      render json: @album
+    else
+      render json: {errors: 'Ya dun goofed!'}
+    end
+  end
+
+  def destroy
+    @album = Album.destroy(params[:id])
+    @albums = Album.all
+      render json:
+        { success: "#{@album[:name]} Destroyed!",
+          favorites: @albums }
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :artists, only: [:index, :create, :destroy]
+      resources :albums, only: [:index, :create, :destroy]
+    end
+  end
+
+
+end
