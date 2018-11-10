a3987a35e2205d8d7bc4d7533286deb471aa1c21
github username <elis.paul7@gmail.com>
2018-11-10 10:39:32 -0500

    FavoriteArtist create method cannot create duplicates

--- app/controllers/api/v1/favorite_artists_controller.rb
+++ app/controllers/api/v1/favorite_artists_controller.rb
@@ -6,19 +6,21 @@ class Api::V1::FavoriteArtistsController < ApplicationController
   end
 
   def create
-    @favoriteArtist = FavoriteArtist.find_or_create_by(
-      name: params['artist']['name'],
-      image: params['artist']['image'][3]['#text'],
-      listeners: params['artist']['stats']['listeners'].to_i,
-      playcount: params['artist']['stats']['playcount'].to_i,
-      bio: params['artist']['bio']['content'],
-      url: params['artist']['url'],
-      mbid: params['artist']['mbid'],
-    )
+    @favoriteArtist = FavoriteArtist.find_by(mbid: params['artist']['mbid'])
 
-
-    if @favoriteArtist.save
+    if @favoriteArtist
       render json: @favoriteArtist
+    elsif !@favoriteArtist
+      @favoriteArtist = FavoriteArtist.find_or_create_by(
+        name: params['artist']['name'],
+        image: params['artist']['image'][3]['#text'],
+        listeners: params['artist']['stats']['listeners'].to_i,
+        playcount: params['artist']['stats']['playcount'].to_i,
+        bio: params['artist']['bio']['content'],
+        url: params['artist']['url'],
+        mbid: params['artist']['mbid'],
+      )
+      render json: {success: @favoriteArtist}
     else
       render json: {errors: 'Ya dun goofed!'}
     end
